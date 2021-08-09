f821d2fa8ea5ff976be8e72e97fedb15e912d099
Nicolas <Nicolaswulff2@gmail.com>
2021-08-09 17:22:58 -0400

    add some global variables and set up some event listener drafts

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let numOfColors = 5
+let array = []
 let container = document.getElementById('RGBValue')
 let colorSelection = document.getElementsByClassName('colors')
 let winMessage = document.getElementById('winMessage')
+
+//Event Listeners
+const color0 = document.getElementById('color0')
+const color1 = document.getElementById('color1')
+const color2 = document.getElementById('color2')
+
+color0.addEventListener('click', playerClick)
+color1.addEventListener('click', playerClick)
+color2.addEventListener('click', playerClick)
