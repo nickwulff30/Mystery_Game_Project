38b62b9c52d46f4093306cd059592b3c66d4a72b
Nicolas <Nicolaswulff2@gmail.com>
2021-08-10 10:59:32 -0400

    tuesday morning

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// numOfColors = 5
+numOfColors = 4
 let array = []
 let picked
 
@@ -7,20 +7,32 @@ let colors = document.getElementsByClassName('colors')
 let winMessage = document.getElementById('winMessage')
 
 //function to start the game.
-function startGame() {
-  colorMix = picked
-}
+// function startGame() {
+//   colorMix = picked
+// }
 
+array = generateRandomColors(4)
+
+picked = arr()
+
+function colors() {}
 //Event Listeners
 const color0 = document.getElementById('color0')
 const color1 = document.getElementById('color1')
 const color2 = document.getElementById('color2')
+const color3 = document.getElementById('color3')
 
 color0.addEventListener('click', playerClick)
 color1.addEventListener('click', playerClick)
 color2.addEventListener('click', playerClick)
+color3.addEventListener('click', playerClick)
 
 //function when you click on the circles:
 
 //RGBs values are numbers on a scale between 0 to 255.
-// let RGBMix1 = (Math.round, Math.
+function generateColor() {
+  let RGBMix1 = Math.floor(Math.random() * 255)
+  let RGBMix2 = Math.floor(Math.random() * 255)
+  let RGBMix3 = Math.floor(Math.random() * 255)
+}
+//https://css-tricks.com/snippets/javascript/random-hex-color/
