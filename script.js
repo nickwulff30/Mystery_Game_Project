09b7a85e5ecc574e0ea952599f707b450ecf7c8e
Nicolas <Nicolaswulff2@gmail.com>
2021-08-09 20:11:48 -0400

    make a list of the functions I think I will need and the variables that need to populate in the browser

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let numOfColors = 5
+// numOfColors = 5
 let array = []
-let container = document.getElementById('RGBValue')
-let colorSelection = document.getElementsByClassName('colors')
+let picked
+
+let colorMix = document.getElementByID('colorMix')
+let colors = document.getElementsByClassName('colors')
 let winMessage = document.getElementById('winMessage')
+
+
+//function to start the game.
+function startGame() {
+  colorMix
+}
 
 //Event Listeners
 const color0 = document.getElementById('color0')
@@ -12,3 +20,8 @@ const color2 = document.getElementById('color2')
 color0.addEventListener('click', playerClick)
 color1.addEventListener('click', playerClick)
 color2.addEventListener('click', playerClick)
+
+//function when you click on the circles:
+
+//RGBs values are numbers on a scale between 0 to 255.
+let RGBMix1 = (Math.round, Math.
